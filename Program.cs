9c53a8cefb461e7ae7ebb858cfd6e0f6539829eb
Vistaar Juneja <vistaar.juneja@harness.io>
2021-10-25 23:22:47 +0530

    Add basic .Net project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SampleDotNetProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
